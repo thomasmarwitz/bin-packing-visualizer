7eefeeccc321ed2809e67f4aac43d7194930219e
thomasmarwitz <thomasmarwitz3@gmail.com>
2022-06-21 09:54:51 +0200

    Added three.js, mui styles.
    Test rotating box.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Box } from './Box'
+import {Button, Card, Grid, Modal, Typography} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+    <Button 
+        variant="contained" 
+        onClick={() => {}}
+    >
+        Agree
+    </Button>
+
+  </>
   );
 }
 
