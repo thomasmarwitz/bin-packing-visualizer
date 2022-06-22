43dd3e1be24f8ca664beebf9b47ad2f7b2e00250
thomasmarwitz <thomasmarwitz3@gmail.com>
2022-06-22 11:29:03 +0200

    Adding boxes interface

--- src/routes/BinPacking.js
+++ src/routes/BinPacking.js
@@ -1,18 +1,24 @@
 import { Box } from "@mui/system";
 import { useDispatch, useSelector } from "react-redux";
-import { selectCurrentBinNumber, selectCurrentPackage, selectNumberTotalBins, setCurrentBin } from "../store/packagingSlice/packagingSlice";
+import { selectCurrentBinNumber, selectCurrentPackage, selectNumberTotalBins, setCurrentBin, selectHasNext } from "../store/packagingSlice/packagingSlice";
 import { Item } from "../helper/styleHelper";
 import { CurrentPackage } from "../view/CurrentPackage/CurrentPackage";
 import { AllPackages } from "../view/AllPackages/AllPackages";
 import { Controls } from "../view/Controls/Controls";
 import { Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 
 export function BinPacking() {
     const box = useSelector(selectCurrentPackage);
     const currentBinNum = useSelector(selectCurrentBinNumber) + 1;
     const totalBinNum = useSelector(selectNumberTotalBins);
+    const hasNext = useSelector(selectHasNext);
+    
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const showBackToHome = currentBinNum >= totalBinNum && !hasNext
 
     return (<>
         <div style={{display: 'grid', gridTemplateColumns: '10% 10% 10%', columnGap: "35%"}}>
@@ -23,7 +29,11 @@ export function BinPacking() {
             >
                 {"<<"}
             </Button>
-            <Typography>{`Bin ${currentBinNum} / ${totalBinNum}`}</Typography>
+            {
+                showBackToHome ? 
+                <Button variant="contained" onClick={() => navigate("/")}>Back Home</Button>
+                : <Typography>{`Bin ${currentBinNum} / ${totalBinNum}`}</Typography>
+            }
             <Button 
                 variant="contained"
                 disabled={currentBinNum >= totalBinNum}
